c1dfe37fa1295f345abd56339190eb37fc7e59e9
QuickRecon <aren.leishman@gmail.com>
2015-08-16 12:02:59 +0800

    Typo

--- Client.py
+++ Client.py
@@ -27,7 +27,7 @@ from time import sleep
 TCP_IP = '192.168.100.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = "Sysetem online"
+MESSAGE = "System online"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
@@ -37,11 +37,11 @@ s.connect((TCP_IP, TCP_PORT))
 #Pygame stuff
 pygame.init()
 #background_colour = (255,255,255)
- 
+
 #screen = pygame.display.set_mode((500, 400), 0, 32)
 #pygame.display.set_caption('Controller Status')
 #screen.fill(background_colour)
- 
+
 #pygame.display.flip()
 # set up fonts
 #basicFont = pygame.font.SysFont(None, 48)
@@ -111,17 +111,17 @@ while True:
 
         #Send positions of all axes [Right stick Vertical, Right Stick Horizontal, Left Stick Vertical, Left Stick Horizontal, Hat Vertical, Hat Horizontal]
         positions = [joystick.get_axis(0), joystick.get_axis(1), joystick.get_axis(2), joystick.get_axis(3), joystick.get_hat(0)[0], joystick.get_hat(0)[1]]
-        
+
         for button in range(0,12):
                 positions.append(joystick.get_button(button))
-        
+
         #s.send(str(positions))
         print (positions)
         encode = str(positions)
-        sendpos = str.encode(encode) 
+        sendpos = str.encode(encode)
         s.send(sendpos)
-        
-        sleep(0.1) #Debounce controls  
+
+        sleep(0.1) #Debounce controls
 
 
 
