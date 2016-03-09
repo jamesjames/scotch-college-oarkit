942beddb9df42a5092c9c1c5407a59cb861c8a9d
Campbell Millar <c_millar@tpg.com.au>
2016-03-09 19:58:29 +0800

    Added in Logger in

--- Java/src/com/scotch/OARKit/java/helpers/Logger.java
+++ Java/src/com/scotch/OARKit/java/helpers/Logger.java
@@ -1,8 +1,33 @@
 package com.scotch.OARKit.java.helpers;
 
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+
 /**
- * Created by Campbell Millar on 9/03/2016.
+ * Created by CAMPBELL MILLAR/ZENOTON STUDIOS on 9/03/2015. ALL LIBRARIES ARE LICENCED UNDER GNU LGPL
  */
-//TO BE DONE - MAYBE WITH Log4j???
 public class Logger {
+    //TODO Move to a JavaFX
+    public static void info(Object object) {
+        String timeStamp = new SimpleDateFormat("[HH:mm:ss] ").format(Calendar.getInstance().getTime());
+        System.out.println((char) 27 + "[32m" + timeStamp + object);
+    }
+
+    public static void error(Object object) {
+        String timeStamp = new SimpleDateFormat("[HH:mm:ss] ").format(Calendar.getInstance().getTime());
+        System.err.println((char) 27 + "[31m" + timeStamp + object);
+    }
+
+    public static void debug(Object object) {
+        String timeStamp = new SimpleDateFormat("[HH:mm:ss] ").format(Calendar.getInstance().getTime());
+        System.out.println((char) 27 + "[34m" + timeStamp + object);
+    }
+
+    public static void warn(Object object) {
+        String timeStamp = new SimpleDateFormat("[HH:mm:ss] ").format(Calendar.getInstance().getTime());
+        System.err.println((char) 27 + "[33m" + timeStamp + object);
+    }
+
 }
