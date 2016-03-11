cbd61c767cd562f77613f1ee4554c6850599071a
campell1212 <c_millar@tpg.com.au>
2016-03-11 08:01:15 +0800

    Change pom.xml for Jenkins

--- Java/src/com/scotch/OARKit/java/Command/BaseCommand.java
+++ Java/src/com/scotch/OARKit/java/Command/BaseCommand.java
@@ -5,14 +5,16 @@ package com.scotch.OARKit.java.Command;
 import java.util.ArrayList;
 
 public enum BaseCommand {
-    ECHO("E",new String[]{"print","echo"},1),
-    BLANK("",new String[]{},0);
+    ECHO("E","Echo",new String[]{"print","echo"},1),
+    BLANK("","", new String[]{},0);
     public String command;
     public String[] alias;
     public int args;
-    BaseCommand(String command, String[] alias, int args){
+    public String localizedName;
+    BaseCommand(String command, String loclocalizedName, String[] alias, int args){
         this.command = command;
         this.alias = alias;
         this.args = args;
+        this.localizedName = loclocalizedName;
     }
 }
