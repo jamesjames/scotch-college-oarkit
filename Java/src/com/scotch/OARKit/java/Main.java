5f402dddc36d5e42c84f68f69d665acd21fef3fb
QuickRecon <aren.leishman@gmail.com>
2016-03-17 17:20:37 +0800

    Add gampad class

--- Java/src/com/scotch/OARKit/java/Main.java
+++ Java/src/com/scotch/OARKit/java/Main.java
@@ -7,6 +7,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
+
 import java.io.IOException;
 import java.util.Properties;
 
---
+++ Java/src/com/scotch/OARKit/java/helpers/Gamepad.java
@@ -0,0 +1,10 @@
+package com.scotch.OARKit.java.helpers;
+
+/**
+ * Created by Aren Leishman on 17/03/16.
+ */
+
+// TODO add imports
+public class Gamepad {
+
+}
