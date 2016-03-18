d14988af4f3b14df4e129ad67ce6ea4ccc45cc6c
QuickRecon <aren.leishman@gmail.com>
2016-03-18 20:51:45 +0800

    Update gamepad class

--- Java/src/com/scotch/OARKit/java/helpers/gamepad.java
+++ Java/src/com/scotch/OARKit/java/helpers/gamepad.java
@@ -1,9 +1,39 @@
 package com.scotch.OARKit.java.helpers;
 
+import com.scotch.OARKit.java.helpers.JInputJoystick;
 import net.java.games.input.Controller;
 
+/**
+ * Created by Aren Leishman on 3/18/16.
+ */
 public class gamepad {
+
+    private static boolean connected = true;
+
+    public int leftstickx;
+    public int leftsticky;
+
+    public int rightstickx;
+    public int rightsticky;
+
+    private int numberOfButtons;
+
     public void gamepad(){
-        JInputJoystick joystick = new JInputJoystick(Controller.Type.STICK, Controller.Type.GAMEPAD);
+        JInputJoystick gamepad = new JInputJoystick(Controller.Type.STICK, Controller.Type.GAMEPAD);
+        numberOfButtons = gamepad.getNumberOfButtons();
+        // Check if the controller was found.
+        if( !gamepad.isControllerConnected() ){
+            connected = false;
+            System.out.println("No controller found!");
+        }
+        System.out.println("Controller found");
+        System.out.println("This controller has " + numberOfButtons + " buttons");
+        while (connected){
+            if( !gamepad.pollController() ) {
+                System.out.println("Controller disconnected!");
+                // Do some stuff.
+            }
+
+        }
     }
 }
