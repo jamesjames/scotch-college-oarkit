b61c6508bab83229cdfd41dba310c626f91bf9c0
Campbell Millar <c_millar@tpg.com.au>
2016-03-23 07:26:53 +0800

    Started Abstract Interpreter

--- Java/src/com/scotch/OARKit/java/Command/ACommand.java
+++ Java/src/com/scotch/OARKit/java/Command/ACommand.java
@@ -4,5 +4,16 @@ package com.scotch.OARKit.java.Command;
  * Created by Campbell Millar on 11/03/2016.
  */
 public abstract class ACommand {
-    //TODO IMPLEMENT THIS IN
+    public String FinalString;
+    public final int argsLength;
+    ACommand(int argsLength){
+        this.argsLength = argsLength;
+    }
+    public abstract void phaseCommand();
+    public void runString(){
+        
+    }
+    public void checkArgs(){
+
+    }
 }
---
+++ Java/src/com/scotch/OARKit/java/Command/Echo.java
@@ -0,0 +1,15 @@
+package com.scotch.OARKit.java.Command;
+
+/**
+ * Created by Campbell Millar on 22/03/2016.
+ */
+public class Echo extends ACommand {
+    Echo() {
+        super(1);
+    }
+
+    @Override
+    public void phaseCommand() {
+
+    }
+}
