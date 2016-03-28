0f1ad8bcf91b3ee32967c827e496aba8f1e35ae2
QuickRecon <aren.leishman@gmail.com>
2016-03-28 17:08:42 +0800

    Ninja patch

--- Java/src/com/scotch/OARKit/java/Controller.java
+++ Java/src/com/scotch/OARKit/java/Controller.java
@@ -3,10 +3,7 @@ package com.scotch.OARKit.java;
 import com.scotch.OARKit.java.Command.BaseCommand;
 import com.scotch.OARKit.java.Command.Commands;
 import com.scotch.OARKit.java.Command.Interpreter;
-import com.scotch.OARKit.java.helpers.GetServerList;
-import com.scotch.OARKit.java.helpers.NetworkManager;
-import com.scotch.OARKit.java.helpers.ServerConnect;
-import com.scotch.OARKit.java.helpers.gamepad;
+import com.scotch.OARKit.java.helpers.*;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
@@ -26,15 +23,12 @@ import java.lang.Thread;
 
 import java.net.SocketException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class Controller implements Initializable, Runnable{
 
     public static boolean running = true;
-    public static boolean connected = false;
+    public static boolean connected = true;
 
     public static ServerConnect serverConnect;
 
@@ -55,9 +49,6 @@ public class Controller implements Initializable, Runnable{
     ToggleButton connectButton;
     public static ToggleButton connectButton1;
     @FXML
-    TextField connectIP;
-    public static TextField connectIP1;
-    @FXML
     ProgressBar StrengthBar;
     @FXML
     Label StrengthDBMLabel;
@@ -73,7 +64,16 @@ public class Controller implements Initializable, Runnable{
     ToggleButton off;
     @FXML
     MenuButton ipSelector;
-    public  static MenuButton ipSelector1;
+    public static MenuButton ipSelector1;
+    @FXML
+    Label nameLabel;
+    public static Label nameLabel1;
+    @FXML
+    Label ipLabel;
+    public static Label ipLabel1;
+    @FXML
+    Label portLabel;
+    public static Label portLabel1;
 
     @FXML
     ProgressBar LeftX;
@@ -96,13 +96,78 @@ public class Controller implements Initializable, Runnable{
     String currentip;
     int currentport;
 
+    public void print(String out) {
+        System.out.println(out);
+    }
+
+    public static void AddConfigToList(String serverName) {
+        MenuItem newServerName =new MenuItem(serverName);
+        newServerName.setId(serverName);
+        newServerName.setOnAction(event -> {
+            Properties props = new Properties();
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            InputStream stream = loader.getResourceAsStream("com/scotch/OARKit/assets/servers/"+serverName+".properties");
+            try {
+                props.load(stream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(props);
+            nameLabel1.setText("Name: "+props.getProperty("serverName"));
+            ipLabel1.setText("IP: "+props.getProperty("serverIP"));
+            portLabel1.setText("Port: "+props.getProperty("serverPort"));
+            if (ServerConnect.connected){
+                ServerDisconnect();
+            }
+        });
+        Platform.runLater(() -> ipSelector1.getItems().addAll(newServerName));
+    }
+
+    public static void ServerDisconnect() {
+        connectButton1.setSelected(false);
+        System.out.println("Closing Socket");
+        serverConnect.socketClose();
+        connectButton1.setText("Connect");
+        connected = false;
+    }
+
+    public void ServerConnect(String name, String ip, String port) {
+        serverConnect = new ServerConnect(ip, port);
+        engine.load("http://"+ip);
+        System.out.println("Connected to new Server "+name+" ("+ip+", "+port+")");
+        connectButton.setText("Disconnect");
+        connected = true;
+    }
+
+    public void ToggleServerConnection(String name, String ip, String port) {
+        if (!ServerConnect.connected){
+            ServerConnect(name, ip, port);
+        }else {
+            ServerDisconnect();
+        }
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        createEvents();
+        try {
+            createEvents();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         ipSelector1 = ipSelector;
-        connectIP1 = connectIP;
         connectButton1 = connectButton;
+        nameLabel1 = nameLabel;
+        ipLabel1 = ipLabel;
+        portLabel1 = portLabel;
         new GetServerList("com/scotch/OARKit/assets/properties/servers.sList");
+        String stringFromFile = GetServerList.stringFromFile;
+        //System.out.println(stringFromFile);
+        String[] serverList = stringFromFile.split("\n");
+        for (int i = 0; i < serverList.length; i++) {
+            print("Server "+(i+1)+": "+serverList[i]);
+            String[] server = serverList[i].split(", ");
+            print((i+1)+" "+String.valueOf(server));
+        }
         try {
             networkManager = new NetworkManager();
         } catch (IOException e) {
@@ -118,9 +183,9 @@ public class Controller implements Initializable, Runnable{
         engine = CameraWebView.getEngine();
         new Thread(this).start();
         if(Main.properties.getProperty("insideDev").equals("true")){
-            System.out.println("Inside Dev Enviroment");
+            //System.out.println("Inside Dev Environment");
             engine.load("http://www.google.com");
-            connectIP.setText("192.168.100.1");
+            //connectIP.setText("192.168.100.1");
             //engine.loadContent("");
             consoleLog.setText("Inside Dev Environment - Console Will Log but Commands will be ignored!!\n");
         }
@@ -130,7 +195,7 @@ public class Controller implements Initializable, Runnable{
         }
     }
 
-    public void createEvents(){
+    public void createEvents() throws IOException {
         restartNginx.setOnAction(event -> new Interpreter("print hello").returnCommand().runCommand());
         StopServer.setOnAction(event -> new Interpreter("stopserver").returnCommand().runCommand());
 
@@ -139,34 +204,19 @@ public class Controller implements Initializable, Runnable{
             consoleTextField.setText("");
         });
 
-        connectButton.setOnAction(event -> {
-            if (!ServerConnect.connected){
-                serverConnect = new ServerConnect(connectIP.getText(), 5006);
-                engine.load("http://"+connectIP.getText());
-                System.out.println("Connected to new Server " + connectIP.getText());
-                connectButton.setText("Disconnect");
-                connected = true;
-            }else {
-                connectButton.setSelected(false);
-                System.out.println("Closing Socket");
-                serverConnect.socketClose();
-                connectButton.setText("Connect");
-                connected = false;
-            }
-        });
-
         addNewConfiguration.setOnAction(event -> {
             try {
                 Stage NetWindow = new Stage();
                 Parent NetRoot = FXMLLoader.load(getClass().getClassLoader().getResource("com/scotch/OARKit/assets/layout/NetworkManager.fxml"));
-                NetWindow.setTitle("NetworkManager");
-                NetWindow.setScene(new Scene(NetRoot, 420, 120));
+                NetWindow.setTitle("Network Manager");
+                NetWindow.setScene(new Scene(NetRoot, 358, 191));
+                NetWindow.setAlwaysOnTop(true);
+                NetWindow.setResizable(false);
                 NetWindow.show();
             } catch (IOException e){
                 e.printStackTrace();
             }
         });
-
 
         File folder = new File("com/scotch/OARKit/assets/servers");
         File[] listOfFiles = folder.listFiles();
@@ -179,52 +229,33 @@ public class Controller implements Initializable, Runnable{
 
         for (int i = 0; i < servers.size(); i++) {
             String serverName=servers.get(i);
-            MenuItem newServerName =new MenuItem(serverName);
-            newServerName.setId(serverName);
-            newServerName.setOnAction(event -> {
-                connectIP.setText(newServerName.getText());
-                if (ServerConnect.connected){
-                    connectButton.setSelected(false);
-                    System.out.println("Closing Socket");
-                    serverConnect.socketClose();
-                    connectButton.setText("Connect");
+            AddConfigToList(serverName);
+        }/**/
+
+        connectButton.setOnAction(event -> {
+            if (!nameLabel.getText().equals("Name:")) {
+                Properties props = new Properties();
+                ClassLoader loader = Thread.currentThread().getContextClassLoader();
+                InputStream stream = loader.getResourceAsStream("com/scotch/OARKit/assets/servers/"+nameLabel.getText().replace("Name: ", "")+".properties");
+                try {
+                    props.load(stream);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            });
-            Platform.runLater(() -> ipSelector.getItems().addAll(newServerName));
-        }
+                ToggleServerConnection(props.getProperty("serverName"), props.getProperty("serverIP"), props.getProperty("serverPort"));
+            } else {
+                print("Please select a server");
+            }
+        });
 
         /*on.setOnAction(event -> {
             on.setDisable(true);
             off.setDisable(false);
         });
-
         off.setOnAction(event -> {
             off.setDisable(true);
             on.setDisable(false);
         });/**/
-
-        /*CancelButton.setOnAction(event -> {
-            NetWindow = (Stage) CancelButton.getScene().getWindow();
-            NetWindow.close();
-        });
-
-        SaveButton.setOnAction(event -> {
-            Properties props = new Properties();
-            props.setProperty("serverName",NameField.getText());
-            props.setProperty("serverIP",IPField.getText());
-            props.setProperty("serverPort",PortField.getText());
-            System.out.println(props);
-            File f = new File(NameField.getText()+".properties");
-            /*try {
-                OutputStream out = new FileOutputStream(f);
-                out.write(props.);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }/**
-            NetWindow = (Stage) SaveButton.getScene().getWindow();
-            NetWindow.close();
-        });/**/
-
     }
 
     @Override
@@ -232,8 +263,8 @@ public class Controller implements Initializable, Runnable{
         while(running) try {
             try{networkManager.update();} catch (SocketException e) {}
             Platform.runLater(() -> StrengthBar.setProgress(networkManager.getSignalStrength()));
-            Platform.runLater(() -> StrengthDBMLabel.setText("Strength: " + networkManager.getRawSignalStrength() + " Dbm"));
-            Platform.runLater(() -> ConnectionType.setText("Connection: " + networkManager.getConnectionType()));
+            Platform.runLater(() -> StrengthDBMLabel.setText("Strength: "+networkManager.getRawSignalStrength()+ " Dbm"));
+            Platform.runLater(() -> ConnectionType.setText("Connection: "+networkManager.getConnectionType()));
 
             if (networkManager.getSignalStrength() == -1){
                 Platform.runLater(() ->StrengthDBMLabel.setVisible(false));
@@ -257,6 +288,11 @@ public class Controller implements Initializable, Runnable{
             if (!connected){
                 Platform.runLater(() -> connectButton.setText("Connect"));
                 Platform.runLater(() -> connectButton.setSelected(false));
+            }
+            if (!consoleTextField.getText().isEmpty()) {
+                sendButton.setDisable(false);
+            } else {
+                sendButton.setDisable(true);
             }
             Thread.sleep(100);
         } catch (InterruptedException e) {
@@ -286,5 +322,5 @@ public class Controller implements Initializable, Runnable{
     }
     public static void disconnectServer(){
         connected = false;
-    }
+    }/**/
 }
