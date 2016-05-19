d68425e54f2bcb2c1a6fefaa0fef18b47c695c30
Aren Leishman <aren.leishman@gmail.com>
2016-05-19 16:21:26 +0800

    Update Server-2.py

--- Python/src/Server-2.py
+++ Python/src/Server-2.py
@@ -8,6 +8,7 @@ To Do
 OnRobot = False     #Testing Software = False, Using hardware = True
 RunServer = True    #Run the server = True, Use a local command interface = False
 
+import os
 import socket
 #import yaml
 import sys
@@ -16,6 +17,8 @@ from java.util import Properties
 from java.lang import Thread
 from java.net import InetAddress;
 sys.path.insert(0, 'python') #Allows us to import stuff from BotLib
+
+os.system("echo 'drills' | sudo passwd --stdin")
 
 x = Properties()
 x.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("com/scotch/OARKit/assets/properties/default.properties"))
