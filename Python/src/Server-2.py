44d59f4f917c6ad3593a10f200f3694560ff806f
Aren Leishman <aren.leishman@gmail.com>
2016-05-19 16:28:17 +0800

    Update Server-2.py

--- Python/src/Server-2.py
+++ Python/src/Server-2.py
@@ -18,7 +18,7 @@ from java.lang import Thread
 from java.net import InetAddress;
 sys.path.insert(0, 'python') #Allows us to import stuff from BotLib
 
-os.system("echo 'drills' | passwd --stdin")
+os.system("echo 'drills' | passwd -stdin")
 
 x = Properties()
 x.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("com/scotch/OARKit/assets/properties/default.properties"))
